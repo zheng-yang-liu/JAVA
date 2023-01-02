c96b5084e8f731d5dc4fef8683744e24a265c9ca
thexu <thexuyan@outlook.com>
2023-01-02 10:16:58 +0800

    Merge remote-tracking branch 'origin/master'

--- src/kaoshi/ceshi.java
+++ src/kaoshi/ceshi.java
@@ -1,4 +1,13 @@
 package kaoshi;
 
+/**
+ * 前
+ *
+ * @author thexu
+ * @date 2023/01/01
+ */
 public class ceshi {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
 }
